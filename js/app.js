429e9706bd7dce68500846a352ef0dd3109ecc65
AngelGaboCC <angelg.chablec@gmail.com>
2023-04-06 14:37:28 -0600

    agregamos spinner

--- js/app.js
+++ js/app.js
@@ -84,6 +84,8 @@ function consultarAPI() {
 
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 
+    mostrarSpinner()
+
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(cotizacion => {
@@ -126,4 +128,19 @@ function limpiarHTML() {
     while(resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
     }
+}
+
+function mostrarSpinner() {
+    limpiarHTML();
+
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner');
+
+    spinner.innerHTML = ` 
+         <div class="bounce1"></div>
+        <div class="bounce2"></div>
+         <div class="bounce3"></div>
+    `
+
+    resultado.appendChild(spinner);
 }
